b13ed713a0efa3eb7cbac3cd3e1d51fc53d1571a
Stacey Oue <stacey.oue@autodesk.com>
2021-01-13 18:06:20 -0500

    Avoid key error if the sg_data does not have a "path" field.

--- python/tk_multi_breakdown2/api/manager.py
+++ python/tk_multi_breakdown2/api/manager.py
@@ -143,7 +143,7 @@ class BreakdownManager(object):
         :param sg_data: Dictionary of Shotgun data representing the published file we want to update the item to
         """
 
-        if not sg_data["path"]["local_path"]:
+        if not sg_data or not sg_data.get("path", {}).get("local_path", None):
             return
 
         item.path = sg_data["path"]["local_path"]
