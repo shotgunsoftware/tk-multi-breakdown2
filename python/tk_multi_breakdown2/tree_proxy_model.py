cf96c42f288d7490134f9e3715c9a5a566c55d38
Stacey Oue <stacey.oue@autodesk.com>
2021-04-16 16:19:07 -0400

    Fully implement the TreeProxyModel. TODO: move this class to qtwidgets framework when ready.

--- python/tk_multi_breakdown2/tree_proxy_model.py
+++ python/tk_multi_breakdown2/tree_proxy_model.py
@@ -17,11 +17,13 @@ from .framework_qtwidgets import HierarchicalFilteringProxyModel
 class TreeProxyModel(HierarchicalFilteringProxyModel):
     """
     A proxy model for source models with a tree data structure.
+
+    TODO: Move this to tk-frameowrk-qtwidgets once the model is more fleshed out.
     """
 
     def __init__(self, parent, filter_items=None):
         """
-        Constructor
+        TreeProxyModel constructor
         """
 
         HierarchicalFilteringProxyModel.__init__(self, parent)
@@ -41,7 +43,12 @@ class TreeProxyModel(HierarchicalFilteringProxyModel):
         self.invalidateFilter()
 
     def _is_row_accepted(self, src_row, src_parent_idx, parent_accepted):
-        """"""
+        """
+        Override the base method.
+
+        Go through the list of filters and check whether or not the src_row
+        is accepted based on the filters.
+        """
 
         src_idx = self.sourceModel().index(src_row, 0, src_parent_idx)
         if not src_idx.isValid():
@@ -51,17 +58,13 @@ class TreeProxyModel(HierarchicalFilteringProxyModel):
             return True  # Accept all group headers for now
 
         if not self.filter_items:
-            return True  # No filters, accept everything
+            return True  # No filters set, accept everything
 
-        accepted = self._do_filter(src_idx, self.filter_items, FilterItem.OP_AND)
-        if accepted:
-            return True
-
-        return False
+        return self._do_filter(src_idx, self.filter_items, FilterItem.OP_AND)
 
     def _do_filter(self, src_idx, filter_items, op):
         """
-        Recursively filter
+        Recursively filter the data for the given src_idx.
         """
 
         if not FilterItem.is_group_op:
@@ -69,18 +72,10 @@ class TreeProxyModel(HierarchicalFilteringProxyModel):
 
         for filter_item in filter_items:
             if filter_item.is_group():
-                self._do_filter(src_idx, filter_item.filters, filter_item.op)
+                self._do_filter(src_idx, filter_item.filters, filter_item.filter_op)
 
             else:
-                # TODO move to filter item class
-                if filter_item.filter_role:
-                    data = src_idx.data(filter_item.filter_role)
-                elif filter_item.data_func:
-                    data = filter_item.data_func(src_idx)
-                else:
-                    data = None
-
-                accepted = filter_item.accepts(data)
+                accepted = filter_item.accepts(src_idx)
 
                 if op == FilterItem.OP_AND and not accepted:
                     return False
@@ -100,9 +95,10 @@ class TreeProxyModel(HierarchicalFilteringProxyModel):
 
 class FilterItem(object):
     """
-    A filter item to pass to the TreeProxyModel.
+    A class object to store all necesasry information for a filter used by the TreeProxyModel.
     """
 
+    # The filter operations
     OP_AND = "and"
     OP_OR = "or"
     OP_IS_TRUE = "true"
@@ -116,12 +112,15 @@ class FilterItem(object):
     OP_GREATER_THAN = ">"
     OP_GREATER_THAN_OR_EQUAL = ">="
 
-    TYPE_GROUP = "group"
+    # The filter types
     TYPE_BOOL = "bool"
     TYPE_STR = "str"
-    TYPE_REGEX_STR = "regex"
-    TYPE_INT = "int"
+    # TYPE_REGEX_STR = "regex"
+    TYPE_NUMBER = "number"
     TYPE_LIST = "list"
+    # The group type is a special type that contains a list of filters itself and its operation
+    # is either AND or OR the list of filters
+    TYPE_GROUP = "group"
 
     def __init__(
         self,
@@ -146,46 +145,86 @@ class FilterItem(object):
         self._filter_funcs_by_type = {
             self.TYPE_BOOL: self.is_bool_valid,
             self.TYPE_STR: self.is_str_valid,
-            self.TYPE_REGEX_STR: self.is_regex_str_valid,
-            self.TYPE_INT: self.is_int_valid,
+            # self.TYPE_REGEX_STR: self.is_regex_str_valid,
+            self.TYPE_NUMBER: self.is_number_valid,
             self.TYPE_LIST: self.is_list_valid,
         }
 
-    def is_bool_valid(self, bool_value):
+    def is_bool_valid(self, value):
         """
         Filter str tupe.
         """
 
-        raise NotImplementedError
+        if self.filter_op == self.OP_IS_TRUE:
+            return value
 
-    def is_str_valid(self, str_value):
+        if self.filter_op == self.OP_IS_FALSE:
+            return not value
+
+        if self.filter_op == self.OP_EQUAL:
+            return value == self.filter_value
+
+        assert False, "Unsupported operation for filter type 'bool'"
+        return False
+
+    def is_str_valid(self, value):
         """
         Filter str tupe.
         """
 
-        raise NotImplementedError
+        if self.filter_op == self.OP_EQUAL:
+            return value == self.filter_value
 
-    def is_regex_str_valid(self, regex_value):
-        """
-        Filter regex str tupe.
-        """
+        if self.filter_op == self.OP_IN:
+            regex = QtCore.QRegularExpression(
+                self.filter_value, QtCore.QRegularExpression.CaseInsensitiveOption
+            )
 
-        match = self.filter_value.match(regex_value)
-        return match.hasMatch()
+            match = regex.match(value)
+            return match.hasMatch()
 
-    def is_int_valid(self, str_value):
-        """
-        Filter str tupe.
-        """
+        assert False, "Unsupported operation for filter type 'str'"
+        return False
 
-        raise NotImplementedError
-
-    def is_list_valid(self, str_value):
+    def is_number_valid(self, value):
         """
         Filter str tupe.
         """
 
-        raise NotImplementedError
+        if self.filter_op == self.OP_EQUAL:
+            return value == self.filter_value
+
+        if self.filter_op == self.OP_GREATER_THAN:
+            return value > self.filter_value
+
+        if self.filter_op == self.OP_GREATER_THAN_OR_EQUAL:
+            return value >= self.filter_value
+
+        if self.filter_op == self.OP_LESS_THAN:
+            return value < self.filter_value
+
+        if self.filter_op == self.OP_LESS_THAN_OR_EQUAL:
+            return value <= self.filter_value
+
+        assert False, "Unsupported operation for filter type 'number'"
+        return False
+
+    def is_list_valid(self, value):
+        """
+        Filter str tupe.
+        """
+
+        if self.filter_op == self.OP_IN:
+            for filter_val in self.filter_value:
+                if value == filter_val:
+                    return True
+            return False
+
+        if self.filter_op == self.OP_EQUAL:
+            return value == self.filter_value
+
+        assert False, "Unsupported operation for filter type 'list'"
+        return False
 
     @classmethod
     def is_group_op(cls, op):
@@ -202,14 +241,28 @@ class FilterItem(object):
 
         return self.filter_type == self.TYPE_GROUP
 
-    def accepts(self, value):
+    def get_index_data(self, index):
         """
-        Return True if this filter item accepts the given value.
+        Return the data for this filter item and index.
         """
 
+        if self.filter_role:
+            return index.data(self.filter_role)
+
+        if self.data_func:
+            return self.data_func(index)
+
+        return None
+
+    def accepts(self, index):
+        """
+        Return True if this filter item accepts the given index.
+        """
+
+        data = self.get_index_data(index)
         filter_func = self._filter_funcs_by_type.get(self.filter_type, None)
 
         if filter_func is None:
             return False  # Invalid filter type
 
-        return filter_func(value)
+        return filter_func(data)
