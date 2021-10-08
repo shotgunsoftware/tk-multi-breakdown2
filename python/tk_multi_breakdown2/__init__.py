207aa1af9e794e54e890c02679666d5dcb8b0056
Rob Aitchison <43424110+rob-aitchison@users.noreply.github.com>
2021-10-08 16:19:36 -0400

    [SHOT-3868] Rename to appear as Breakdown. (#24)
    * Rename to appear as Breakdown.
    Updated icon.
    info.yml updates.
    
    * Updated test_app.py for previous commit.
    
    * Updated app_test_base.py for previous commit.
    
    * Updated app_test_base.py for previous commit.
    
    * Fixed info.yml for unit_tests

--- python/tk_multi_breakdown2/__init__.py
+++ python/tk_multi_breakdown2/__init__.py
@@ -21,4 +21,4 @@ def show_dialog(app):
     # defer imports so that the app works gracefully in batch modes
     from .dialog import AppDialog
 
-    app.engine.show_dialog("Breakdown2", app, AppDialog)
+    app.engine.show_dialog("Scene Breakdown", app, AppDialog)
