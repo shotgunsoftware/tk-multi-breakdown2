0a8af643feaa7ebf23e6736e6f8f332697ea9e14
unknown <stacey.oue@autodesk.com>
2023-12-15 12:26:31 -0500

    Remove unused import

--- python/tk_multi_breakdown2/actions.py
+++ python/tk_multi_breakdown2/actions.py
@@ -8,8 +8,6 @@
 # agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
 # not expressly granted therein are reserved by Autodesk, Inc.
 
-import copy
-
 import sgtk
 from sgtk.platform.qt import QtCore, QtGui
 
